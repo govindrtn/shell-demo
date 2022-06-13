2e29e4bacaa9e54d4ebbc81dc45eb6761569cd25
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-13 11:05:49 +0530

    added new feature Subtraction

--- first-p.sh
+++ first-p.sh
@@ -1,9 +1,20 @@
+
+
 echo "hello world"
 
-num1 =43
-num2 =43
+num1=43
+num2=43
 
-sum =$((num1 + num2))
+sum=$((num1+num2))
 
 echo $sum
 
+/////"Subtraction"/////
+
+
+num1=43
+num2=43
+
+sub=$((num1-num2))
+
+echo $sub
